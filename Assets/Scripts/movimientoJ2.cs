7d4f17f47b128eadaad7fe50431898a6468cf974
Pedro Marin <pmarinr@gail.com>
2017-01-10 16:58:44 +0100

    #4 Movimiento Futbol II
     Crear un script para el jugador 2 que mediante (AWSD) mueva el otro
    jugador.

---
+++ Assets/Scripts/movimientoJ2.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoJ2 : MonoBehaviour {
+
+	public int velocidad = 10;
+	int vertical = 0;
+	int horizontal = 0;
+	Vector2 v_movimiento;
+
+	Rigidbody2D rb2d;
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.W)) {
+			vertical = 1;
+		}
+
+		if (Input.GetKeyUp (KeyCode.W)) {
+			vertical = 0;
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			vertical = -1;
+		}
+
+		if (Input.GetKeyUp (KeyCode.S)) {
+			vertical = 0;
+		}
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			horizontal = -1;
+		}
+
+		if (Input.GetKeyUp (KeyCode.A)) {
+			horizontal = 0;
+		}
+
+		if (Input.GetKeyDown (KeyCode.D)) {
+			horizontal = 1;
+		}
+
+		if (Input.GetKeyUp (KeyCode.D)) {
+			horizontal = 0;
+		}
+
+		rb2d.velocity = new Vector2(horizontal,vertical) * velocidad ;
+
+	}
+}
