2f99355830a4da64f5eef0bf0567b4af3b4e5fe0
Pedro Marin <pmarinr@gail.com>
2017-01-10 20:27:16 +0100

    #5 Impulsor Nave
    Crea un script en la nave que al pulsar la tecla espacio le aplique una
    fuerza hacia arriba.

---
+++ Assets/Scripts/Nave/Nave.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nave : MonoBehaviour {
+
+	public float impulso = 100f;
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Space)){
+			rb.AddForce(transform.up * 100f);
+		}
+	}
+}
