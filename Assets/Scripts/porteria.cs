f04150f32069b2ac6bd636c0a364d0846eb726d4
Pedro Marin <pmarinr@gail.com>
2017-01-10 17:21:10 +0100

    #7 Marcador
    Crea un script en cada portería para que sume uno cada vez que el balón
    toque la portería.

---
+++ Assets/Scripts/porteria.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class porteria : MonoBehaviour {
+
+	public Text marcador;
+	public string nombre = "Rosa";
+	int goles = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.transform.name == "balon") {
+			goles += 1;
+			marcador.text = goles + " " + nombre;
+		}
+	}
+}
