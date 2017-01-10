ac2bb141198f89fe482f708a646bae5d90bca1e4
Pedro Marin <pmarinr@gail.com>
2017-01-10 17:44:30 +0100

    #11 Explosión Nave
    Crea un sistema de partículas para que explote la nave.

---
+++ Assets/Scripts/Nave/explota.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explota : MonoBehaviour {
+	public GameObject explosion;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		Instantiate (explosion, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+}
