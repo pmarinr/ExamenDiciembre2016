534cc298e22c8561216a8d3a410f73a56726aac2
Pedro Marin <pmarinr@gail.com>
2017-01-13 13:40:56 +0100

    #18 Naves Enemigas
    Crea una nave enemiga que se mueva horizontalmente y que cada vez que
    el jugador pase por debajo dispare un láser.

---
+++ Assets/Scripts/Robot/EnemigoScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoScript : MonoBehaviour {
+	public GameObject laser;
+	public Transform punto_disparo;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.transform.tag == "Player") {
+			Instantiate (laser,punto_disparo.position,punto_disparo.rotation);
+		}
+	}
+}
---
+++ Assets/Scripts/Robot/LaserEnemigoScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserEnemigoScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 2);
+	}
+	
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.transform.tag == "Player") {
+			Destroy (col.gameObject);
+		}
+		Destroy (gameObject);
+	}
+}
