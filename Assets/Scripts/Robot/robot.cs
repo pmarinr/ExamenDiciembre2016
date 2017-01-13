aac4c609ae3233537c2783670d9798832a158f5e
Pedro Marin <pmarinr@gail.com>
2017-01-13 12:57:19 +0100

    #16 Movimiento Robot
    Crea un script para mover el robot hacia los lados usando las flechas.

---
+++ Assets/Scripts/Robot/robot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robot : MonoBehaviour {
+	Rigidbody2D rb;
+	SpriteRenderer sprite;
+	public float velocidad = 5f;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		sprite = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			rb.velocity = new Vector2 (-velocidad, 0);
+			sprite.flipX = true;
+		}
+
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rb.velocity = new Vector2 (velocidad,0);
+			sprite.flipX = false;
+		}
+		
+	}
+}
